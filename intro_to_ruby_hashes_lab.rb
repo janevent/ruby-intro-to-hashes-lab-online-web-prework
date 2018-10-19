80a39d0bab1872027505d40c10a237978d4e14b3
Jane Venter <janeanneventer@gmail.com>
2018-10-19 17:19:59 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
   monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {:reading_railroad => {},
-	    :pennsylvanis_railroad => {},
+	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
 	    :shortline => {}
 	  },
